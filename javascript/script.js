d73853c98db21b45047d87e76c34b81d6e28b56d
Urioxi <urioxa@gmail.com>
2022-11-17 19:11:19 +0100

    1st var

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,16 @@
+//first function
 function changetxt() {
     document.getElementById("txt").innerHTML = "Ce texte a été changé via une fonction !";
 }
+//statements
 let x, y, z;
 x = 5;
 y = 5;
 z = x + y;
+
+//var / let
+var variable;
+let varilet, varilet2;
+varilet = 10;
+varilet2 = 12;
+variable = varilet + varilet2;
